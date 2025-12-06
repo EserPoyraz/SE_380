1e7d13c369768f3459f79d432ff59493dda9121a
Eser Poyraz <eserela35@gmail.com>
2025-12-06 22:29:21 +0300

    Basic Step Counter and Water Tracker added

--- group_project_app/lib/main.dart
+++ group_project_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() {
   runApp(const MainApp());
@@ -17,24 +19,25 @@ class MainApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  
+   
   const HomePage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+    int stepCount = Random().nextInt(9000)+1000; // Random step count between 1000 and 9999
     return Scaffold(
       appBar: AppBar(title: const Text("FitnessApp")),
       body: ListView(
         padding: const EdgeInsets.all(24), // spacing
         children: [
-          NavigationBox(
-            // default : new page
-            title: "box 1",
+          StepCounterBox(
+            steps : stepCount,
             color: Colors.blueAccent,
-            destination: const DetailPage(title: "box 1"),
           ),
           const SizedBox(height: 24),
-
-          const CounterBox(color: Colors.greenAccent), // default : counter +
+          WaterTrackerBox(color: Colors.cyanAccent), // default : counter +
           const SizedBox(height: 24),
 
           NavigationBox(
@@ -154,6 +157,123 @@ class DetailPage extends StatelessWidget {
           "Burası $title",
           style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
         ),
+      ),
+    );
+  }
+}
+class StepCounterBox extends StatelessWidget{
+  final int steps;
+  final Color color;
+  const StepCounterBox({super.key, required this.steps, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (_) => StepCounterPage(steps: steps),
+        ))  ;
+      },
+      child: Container(
+        height: 170,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(22),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          "Steps: $steps",
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold
+        ),
+        ),
+      ),
+    );
+  }
+}
+class StepCounterPage extends StatelessWidget{
+  final int steps;
+  final int stepGoal = 10000;
+  const StepCounterPage({super.key, required this.steps});
+
+  @override
+  Widget build(BuildContext context) {
+   double progress = steps / stepGoal;
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Step Counter")),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  width: 180,
+                  height: 180,
+                  child: CircularProgressIndicator(
+                    value: progress.clamp(0,1),
+                    strokeWidth: 14,
+                    backgroundColor: Colors.grey.shade300,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+
+                Text(
+                  "$steps / $stepGoal",
+                  style: const TextStyle(
+                    fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            Text(
+              "%${(progress * 100).toStringAsFixed(1)} completed",
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
+class WaterTrackerBox extends StatefulWidget {
+  final Color color;
+
+  const WaterTrackerBox({super.key, required this.color});
+
+  @override
+  State<WaterTrackerBox> createState() => _WaterTrackerBoxState();
+}
+class _WaterTrackerBoxState extends State<WaterTrackerBox> {
+  int water = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 170,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: widget.color,
+        borderRadius: BorderRadius.circular(22),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Water : $water ml",
+            style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                water+=200;
+              });
+            },
+            child: const Text("Drink💧(200 ml)"),
+          ),
+        ],
       ),
     );
   }
